c2299790203fb7f7702918dddc80e8d656a3ced3
HP <HP@HP-PC>
2016-04-29 12:49:40 -0500

    Version inicial del programa de cifrado

--- src/DAO/FirmServicies.java
+++ src/DAO/FirmServicies.java
@@ -4,14 +4,14 @@ package DAO;
 
 public class FirmServicies {
     
-  public String desencriptar(string texto){
+  public String desencriptar(String texto){
       String  resultado="";
       for (int i = 0; i < texto.length(); i++) {
           int letra= (((int)texto.charAt(i))-3);
           if(letra < 65){
               letra +=26;
           }
-          resutado+=(char)letra;
+          resultado+=(char)letra;
       }
       return resultado;
   }  
